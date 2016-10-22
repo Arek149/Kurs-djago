574b88afcf0bdaa43b081a6f3197d40bc18a7863
Arek149 <arek_krajewski@op.pl>
2016-10-23 01:36:58 +0200

    po 6

--- biblio/settings.py
+++ biblio/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shelf',
+    'contact',
+    'rental',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,7 @@ USE_TZ = True #timezone
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATES_DIRS = {
+    os.path.join(BASE_DIR, 'templates'),
+}
